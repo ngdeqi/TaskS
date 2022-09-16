30961b37782966f0416ff3197b68a3e3ce6e1d1c
ngdeqi <deqi.angel@gmail.com>
2022-09-16 17:57:20 +0800

    Create merge conflict

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -15,7 +15,6 @@ public class Main {
         DataManager dm = new DataManager("./data/data.txt");
         ArrayList<Task> tasksData = dm.loadData();
 
-        System.out.println("Printing deadlines");
         printDeadlines(tasksData);
 
         System.out.println("Total number of deadline(s): " + countDeadlines(tasksData));
